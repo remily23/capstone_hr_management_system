d535c3628bfd5d90414800c84cdfa5c420d449f9
Emily Reid <emily.reid31@gmail.com>
2024-01-12 10:43:17 +0000

    added findAllPositions method in PositionController, setup PositionDTO

--- server/Capstone-Project/src/main/java/com/server/Capstone/Project/controllers/PositionController.java
+++ server/Capstone-Project/src/main/java/com/server/Capstone/Project/controllers/PositionController.java
@@ -5,10 +5,9 @@ import com.server.Capstone.Project.services.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("positions")
@@ -21,5 +20,11 @@ public class PositionController {
     public ResponseEntity<Position>createPosition(@RequestBody Position position){
         return new ResponseEntity<>(positionService.newPosition(position), HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Position>>findAllPositions(){
+        return new ResponseEntity<>(positionService.findAllPositions(), HttpStatus.OK);
+    }
+
 
 }
--- server/Capstone-Project/src/main/java/com/server/Capstone/Project/services/PositionService.java
+++ server/Capstone-Project/src/main/java/com/server/Capstone/Project/services/PositionService.java
@@ -1,10 +1,13 @@
 package com.server.Capstone.Project.services;
 
+import com.server.Capstone.Project.models.Employee;
 import com.server.Capstone.Project.models.Position;
 import com.server.Capstone.Project.respositories.EmployeeRepository;
 import com.server.Capstone.Project.respositories.PositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PositionService {
@@ -18,4 +21,11 @@ public class PositionService {
     public Position newPosition(Position position) {
         return positionRepository.save(position);
     }
+
+    public List<Position> findAllPositions() {
+        return positionRepository.findAll();
+    }
+
+
+
 }
