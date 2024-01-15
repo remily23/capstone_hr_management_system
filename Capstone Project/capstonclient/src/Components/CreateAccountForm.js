87d9d064bb724923f32ab919c3fa779fe394421a
RyanNekadio <ryanekadio12@hotmail.co.uk>
2024-01-15 11:43:46 +0000

    fixed formatting in container and component classes

--- Capstone Project/capstonclient/src/Components/CreateAccountForm.js
+++ Capstone Project/capstonclient/src/Components/CreateAccountForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-const CreateAccountForm = ({currentUser, postEmployee}) => {  
-    const[stateEmployee, setStateEmployee] = useState({
+const CreateAccountForm = ({ currentUser, postEmployee }) => {
+    const [stateEmployee, setStateEmployee] = useState({
         firstName: "",
         lastName: "",
         email: "",
@@ -16,8 +16,8 @@ const CreateAccountForm = ({currentUser, postEmployee}) => {
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
-        if(
-            stateEmployee.firstName === "" || 
+        if (
+            stateEmployee.firstName === "" ||
             stateEmployee.lastName === "" ||
             stateEmployee.email === "" ||
             stateEmployee.password === "" ||
@@ -26,60 +26,60 @@ const CreateAccountForm = ({currentUser, postEmployee}) => {
             stateEmployee.phoneNumber === "" ||
             stateEmployee.proRota === "" ||
             stateEmployee.salary === ""
-        ){
+        ) {
             alert("Please fill in the sign up form")
             return
         }
-            postEmployee(stateEmployee)
-            setStateEmployee({
-                firstName: "",
-                lastName: "",
-                email: "",
-                password: "",
-                dateOfBirth: "",
-                address: "",
-                phoneNumber: "",
-                proRota: "",
-                salary: ""
-            })
+        postEmployee(stateEmployee)
+        setStateEmployee({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            dateOfBirth: "",
+            address: "",
+            phoneNumber: "",
+            proRota: "",
+            salary: ""
+        })
     }
 
     const handleChange = (e) => {
         let fieldName = e.target.name;
-        let copiedEmployee = {...stateEmployee};
+        let copiedEmployee = { ...stateEmployee };
         copiedEmployee[fieldName] = e.target.value;
         setStateEmployee(copiedEmployee);
     }
 
-    return ( 
+    return (
 
-    <>
-    <h1>CreateAccountPage</h1>
-    <div>
-        <form onSubmit={handleFormSubmit}>
-            {currentUser && (
-            <p>Access Level 
-                <select id="dropdown">
-                    <option>Admin</option> 
-                    <option>Employee</option>
-                </select>
-                <label>First Name: </label><input type="text" onChange={handleChange} name="firstName" placeholder="Enter your first name" value={stateEmployee.firstName}/>
-                <label>Last Name: </label><input type="text" onChange={handleChange} name="lastName" placeholder="Enter your last name" value={stateEmployee.lastName}/>
-                <label>Email Address: </label><input type="email" onChange={handleChange} name="email" placeholder="Enter your email address" value={stateEmployee.email}/>
-                <label>Password: </label><input type="password" onChange={handleChange} name="password" placeholder="Enter your password" value={stateEmployee.password}/>
-                <label>Date of Birth: </label><input type="date" onChange={handleChange} name="dateOfBirth" placeholder="Enter your date of birth" value={stateEmployee.dateOfBirth}/>
-                <label>Address: </label><input type="text" onChange={handleChange} name="address" placeholder="Enter your address" value={stateEmployee.address}/>
-                <label>Phone Number: </label><input type="tel" onChange={handleChange} name="phoneNumber" placeholder="Enter your phone number" value={stateEmployee.phoneNumber}/>
-                <label>Pro Rota: </label><input type="text" onChange={handleChange} name="proRota" placeholder="Enter your pro rota" value={stateEmployee.proRota}/>
-                <label>Salary: </label><input type="text" onChange={handleChange} name="salary" placeholder="Enter your salary" value={stateEmployee.salary}/>
-                <input type="submit" value="Create"/>
-            </p>
-        )} 
-        </form>
-    </div>
+        <>
+            <h1>CreateAccountPage</h1>
+            <div>
+                <form onSubmit={handleFormSubmit}>
+                    {currentUser && (
+                        <p>Access Level
+                            <select id="dropdown">
+                                <option>Admin</option>
+                                <option>Employee</option>
+                            </select>
+                            <label>First Name: </label><input type="text" onChange={handleChange} name="firstName" placeholder="Enter your first name" value={stateEmployee.firstName} />
+                            <label>Last Name: </label><input type="text" onChange={handleChange} name="lastName" placeholder="Enter your last name" value={stateEmployee.lastName} />
+                            <label>Email Address: </label><input type="email" onChange={handleChange} name="email" placeholder="Enter your email address" value={stateEmployee.email} />
+                            <label>Password: </label><input type="password" onChange={handleChange} name="password" placeholder="Enter your password" value={stateEmployee.password} />
+                            <label>Date of Birth: </label><input type="date" onChange={handleChange} name="dateOfBirth" placeholder="Enter your date of birth" value={stateEmployee.dateOfBirth} />
+                            <label>Address: </label><input type="text" onChange={handleChange} name="address" placeholder="Enter your address" value={stateEmployee.address} />
+                            <label>Phone Number: </label><input type="tel" onChange={handleChange} name="phoneNumber" placeholder="Enter your phone number" value={stateEmployee.phoneNumber} />
+                            <label>Pro Rota: </label><input type="text" onChange={handleChange} name="proRota" placeholder="Enter your pro rota" value={stateEmployee.proRota} />
+                            <label>Salary: </label><input type="text" onChange={handleChange} name="salary" placeholder="Enter your salary" value={stateEmployee.salary} />
+                            <input type="submit" value="Create" />
+                        </p>
+                    )}
+                </form>
+            </div>
 
-    
-    </>
- );
+
+        </>
+    );
 }
- export default CreateAccountForm ; 
+export default CreateAccountForm; 
--- Capstone Project/capstonclient/src/Containers/DashBoardContainer.js
+++ Capstone Project/capstonclient/src/Containers/DashBoardContainer.js
@@ -3,40 +3,40 @@ import CreateAccountForm from "../Components/CreateAccountForm";
 
 const DashBoardContainer = () => {
 
- const[currentUser, setCurrentUser] = useState("");
- const [stateEmail, setStateEmail] = useState("");
- const [listOfEmployees, setListOfEmployees] = useState([]);
+   const [currentUser, setCurrentUser] = useState("");
+   const [stateEmail, setStateEmail] = useState("");
+   const [listOfEmployees, setListOfEmployees] = useState([]);
 
- const fetchEmployee = async() => {
-    const response =  await fetch ("http://localhost:8080/employees")
-    const data = await response.json()
-    setCurrentUser(data)
- }
+   const fetchEmployee = async () => {
+      const response = await fetch("http://localhost:8080/employees")
+      const data = await response.json()
+      setCurrentUser(data)
+   }
 
- const postEmployee = async(newEmployee) => {
-   const response = await fetch ("http://localhost:8080/employees", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(newEmployee),
-   })
-   const addEmployee = await response.json()
-   setListOfEmployees([...listOfEmployees, addEmployee]);
- }
+   const postEmployee = async (newEmployee) => {
+      const response = await fetch("http://localhost:8080/employees", {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify(newEmployee),
+      })
+      const addEmployee = await response.json()
+      setListOfEmployees([...listOfEmployees, addEmployee]);
+   }
 
 
 
- useEffect (()=> {
+   useEffect(() => {
 
-   fetchEmployee();
- }, [])
+      fetchEmployee();
+   }, [])
 
-    return ( 
+   return (
 
-        <>
-        <h2>Welcome Page</h2>
-        <CreateAccountForm currentUser= {currentUser[0]} postEmployee= {postEmployee}/>
-        </>
-     );
+      <>
+         <h2>Welcome Page</h2>
+         <CreateAccountForm currentUser={currentUser[0]} postEmployee={postEmployee} />
+      </>
+   );
 }
- 
+
 export default DashBoardContainer;
