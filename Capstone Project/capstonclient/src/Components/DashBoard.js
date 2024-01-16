a3d0b5e9b6f1e7d5e31dba686c2899b339ee627b
Aanis Nuur <Aanisn10@gmail.com>
2024-01-16 10:31:18 +0000

    routes - redirect to login page when on website and when user is logged in navigate to dashboard page

--- Capstone Project/capstonclient/src/Components/DashBoard.js
+++ Capstone Project/capstonclient/src/Components/DashBoard.js
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const DashBoard= () => {
-    return (
-        <h1>Homepage</h1>
-        
-      );
-}
- 
+const DashBoard = () => {
+  return <h1>DashBoard page</h1>;
+};
+
 export default DashBoard;
