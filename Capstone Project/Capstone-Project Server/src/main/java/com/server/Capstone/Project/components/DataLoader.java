aa65b17e863f11f62cecb91b142b1a908fa6ef72
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2024-01-16 14:41:48 +0000

    Populated dataLoader with more user information

--- Capstone Project/Capstone-Project Server/src/main/java/com/server/Capstone/Project/components/DataLoader.java
+++ Capstone Project/Capstone-Project Server/src/main/java/com/server/Capstone/Project/components/DataLoader.java
@@ -22,29 +22,75 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         Employee employee1 = new Employee(
                 "Aanis",
-                "Nuur",
+                "N",
                 "aanisn10@gmail.com",
                 "aanis10",
                 "2000-09-29",
-                "aanisn10@gmail.com",
+                "SW3 5GH",
                 "012345678910",
                 7989,
                 2000
         );
 
         Employee employee2 = new Employee(
-                "tom",
-                "hanks",
+                "Tom",
+                "Hanks",
                 "tomhanks@gmail.com",
                 "tomhanks",
                 "1956-07-09",
-                "tomhanks@gmail.com",
+                "BN2 3TA",
                 "109384374839",
                 12,
                 22
         );
 
 
+
+        Employee employee3 = new Employee(
+                "Saima",
+                "M",
+                "saima@bnta.com",
+                "123",
+                "2000-01-01",
+                "BN3 4TA",
+                "123456789",
+                13,
+                33
+        );
+
+        Employee employee4 = new Employee(
+                "Emily",
+                "R",
+                "emily@bnta.com",
+                "123",
+                "2000-02-02",
+                "BN4 5TA",
+                "3456678991",
+                14,
+                34
+        );
+        Employee employee5 = new Employee(
+                "Ryan",
+                "N",
+                "ryan@bnta.com",
+                "123",
+                "2000-03-03",
+                "BN5 6TA",
+                "321123456",
+                15,
+                35
+        );
+        Employee employee6 = new Employee(
+                "Gisele",
+                "A",
+                "gisel@bnta.com",
+                "123",
+                "2000-04-04",
+                "BN6 7TA",
+                "456654321",
+                16,
+                36
+        );
 
         Position position = new Position(
                 "HR department",
@@ -53,6 +99,11 @@ public class DataLoader implements ApplicationRunner {
 
         employeeRepository.save(employee1);
         employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
+        employeeRepository.save(employee4);
+        employeeRepository.save(employee5);
+        employeeRepository.save(employee6);
+
         employee1.setPosition(position);
         employee2.setPosition(position);
 //        position.addEmployee(employee1);
