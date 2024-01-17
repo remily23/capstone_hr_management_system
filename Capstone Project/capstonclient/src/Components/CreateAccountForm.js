90898835598b4edc99af1d3e151a8bb7196c1fa2
Emily Reid <emily.reid31@gmail.com>
2024-01-17 16:48:13 +0000

    started initial CSS for create new account form

--- Capstone Project/capstonclient/src/Components/CreateAccountForm.js
+++ Capstone Project/capstonclient/src/Components/CreateAccountForm.js
@@ -50,27 +50,38 @@ const CreateAccountForm = ({ currentUser, postEmployee }) => {
     }
     return (
         <>
-            <h2>Create Account Page</h2>
-            <div>
+        
+        <section className="create-account">
+            <h2>Create Account</h2>
                 <form onSubmit={handleFormSubmit}>
-                        <p>Access Level
+                        Access Level
                             <select id="dropdown">
                                 <option>Admin</option>
                                 <option>Employee</option>
                             </select>
+
                             <label>First Name: </label><input type="text" onChange={handleChange} name="firstName" placeholder="Enter your first name" value={stateEmployee.firstName} />
+
                             <label>Last Name: </label><input type="text" onChange={handleChange} name="lastName" placeholder="Enter your last name" value={stateEmployee.lastName} />
+
                             <label>Email Address: </label><input type="email" onChange={handleChange} name="email" placeholder="Enter your email address" value={stateEmployee.email} />
+
                             <label>Password: </label><input type="password" onChange={handleChange} name="password" placeholder="Enter your password" value={stateEmployee.password} />
+                            
                             <label>Date of Birth: </label><input type="date" onChange={handleChange} name="dateOfBirth" placeholder="Enter your date of birth" value={stateEmployee.dateOfBirth} />
+                            
                             <label>Address: </label><input type="text" onChange={handleChange} name="address" placeholder="Enter your address" value={stateEmployee.address} />
+                            
                             <label>Phone Number: </label><input type="tel" onChange={handleChange} name="phoneNumber" placeholder="Enter your phone number" value={stateEmployee.phoneNumber} />
+
                             <label>Pro Rota: </label><input type="text" onChange={handleChange} name="proRota" placeholder="Enter your pro rota" value={stateEmployee.proRota} />
-                            <label>Salary: </label><input type="text" onChange={handleChange} name="salary" placeholder="Enter your salary" value={stateEmployee.salary} />
+
+                           <label>Salary: </label><input type="text" onChange={handleChange} name="salary" placeholder="Enter your salary" value={stateEmployee.salary} />
+                            
                             <input type="submit" value="Create" />
-                        </p>
                 </form>
-            </div>
+                </section>
+
         </>
     );
 }
