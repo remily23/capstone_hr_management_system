2b28c6eabdd5f1373ef1e78d5829e1fe494f2e89
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2024-01-17 23:42:52 +0000

    Added minor changes in the profile page

--- Capstone Project/capstonclient/src/Components/ProfilePage.js
+++ Capstone Project/capstonclient/src/Components/ProfilePage.js
@@ -2,33 +2,33 @@ import { useNavigate, Outlet } from 'react-router-dom';
 import { Alert, Stack } from "@mui/material";
 import AlertTitle from '@mui/material/AlertTitle';
 
-const ProfilePage = ({currentUser}) => {
+const ProfilePage = ({ currentUser }) => {
     const navigate = useNavigate();
 
     const toUpdateEmployeeForm = () => {
-        navigate('/profile/updateDetails', {state: {currentUser}})
+        navigate('/profile/updateDetails', { state: { currentUser } })
     }
-    return ( 
-    <>
-        <h3>Profile Page</h3>
-        <p>First name: {currentUser.firstName}</p>
-        <p>Last name: {currentUser.lastName}</p>
-        <p>Email: {currentUser.email}</p>
-        <p>Date of birth: {currentUser.dateOfBirth}</p>
-        <p>Address: {currentUser.address}</p>
-        <p>Phone number: {currentUser.phoneNumber}</p>
-        <p>Pro rota: {currentUser.proRota}</p>
-        <p>Salary: {currentUser.salary}</p>
-        <button onClick={toUpdateEmployeeForm}>Edit</button>
-        <Stack sx={{ width: '100%' }} spacing={2}>
-        <Alert severity="success">
-          <AlertTitle>Success</AlertTitle>
-          Account deleted
-        </Alert>
-      </Stack>
+    return (
+        <>
+            <h3>Profile Page</h3>
+            <p>First name: {currentUser.firstName}</p>
+            <p>Last name: {currentUser.lastName}</p>
+            <p>Email: {currentUser.email}</p>
+            <p>Date of birth: {currentUser.dateOfBirth}</p>
+            <p>Address: {currentUser.address}</p>
+            <p>Phone number: {currentUser.phoneNumber}</p>
+            <p>Pro rota: {currentUser.proRota}</p>
+            <p>Salary: {currentUser.salary}</p>
+            <button onClick={toUpdateEmployeeForm}>Edit</button>
+            <Stack sx={{ width: '100%' }} spacing={2}>
+                <Alert severity="success">
+                    <AlertTitle>Success</AlertTitle>
+                    Account deleted
+                </Alert>
+            </Stack>
         </>
-     );
-     
+    );
+
 }
- 
+
 export default ProfilePage;
