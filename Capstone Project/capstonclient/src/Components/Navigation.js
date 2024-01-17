10db216d7931b0d7b70d8bd8a3651fd353e63e96
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2024-01-17 22:28:54 +0000

    Made  minor changes to the navigaion.js file

--- Capstone Project/capstonclient/src/Components/Navigation.js
+++ Capstone Project/capstonclient/src/Components/Navigation.js
@@ -13,69 +13,7 @@ const Navigation = () => {
 };
 export default Navigation;
 
-// import React, { useState } from "react";
-// import Drawer from "@mui/material/Drawer";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemText from "@mui/material/ListItemText";
-// import { Link } from "react-router-dom";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import { Avatar, Button, CssBaseline, Typography } from "@mui/material";
-// import HomeIcon from "@mui/icons-material/Home";
-// import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-// import LogoutIcon from "@mui/icons-material/Logout";
-// import PersonAddIcon from "@mui/icons-material/PersonAdd";
 
-// const SideBar = () => {
-//   const [drawerOpen, setDrawerOpen] = useState(false);
-
-//   const toggleDrawer = (open) => () => {
-//     setDrawerOpen(open);
-//   };
-//   const menuItems = [
-//     { text: "Dashboard", link: "/dashboard", icon: <HomeIcon /> },
-//     { text: "Profile", link: "/profile", icon: <PersonOutlineIcon /> },
-//     { text: "Create Account", link: "/newaccount", icon: <PersonAddIcon /> },
-//     { text: "Sign-out", link: "/login", icon: <LogoutIcon /> },
-//   ];
-//   return (
-//     <div style={{ position: "fixed", left: 10, top: 20 }}>
-//       <Button
-//         onClick={toggleDrawer(true)}
-//         startIcon={<MenuIcon />}
-//         variant="contained"
-//         color="primary"
-//       >
-//         Menu
-//       </Button>
-
-
-//       <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-//         <List>
-//           {menuItems.map((item, index) => (
-//             <ListItem key={index} button onClick={toggleDrawer(false)}>
-//               <ListItemButton>
-//                 <Avatar sx={{ bgcolor: "secondary.main", mr: 2 }}>
-// //                     {item.icon}
-// //              </Avatar>
-
-//                 <Link
-//                   to={item.link}
-//                   style={{ textDecoration: "none", color: "inherit" }}
-//                 >
-
-//                   <ListItemText primary={item.text} />
-//                 </Link>
-//               </ListItemButton>
-//             </ListItem>
-//           ))}
-//         </List>
-//       </Drawer>
-//     </div>
-//   );
-// };
-// export default SideBar;
 
 
 
