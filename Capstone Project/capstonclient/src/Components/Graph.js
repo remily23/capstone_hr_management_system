871fab951a6b8c678a4c7018864063a38c5122c0
Saima Miah <saima_miah@hotmail.co.uk>
2024-01-18 12:45:15 +0000

    changed age field to email

--- Capstone Project/capstonclient/src/Components/Graph.js
+++ Capstone Project/capstonclient/src/Components/Graph.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
+
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'Email',
+    headerName: 'Email',
+    type: 'string',
     width: 90,
   },
   {
@@ -23,8 +24,8 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 1, lastName: 'Hanks', firstName: 'Jon', email: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', email: 42 },
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
@@ -38,6 +39,7 @@ export default function DataTable() {
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
+    
         rows={rows}
         columns={columns}
         initialState={{
@@ -47,7 +49,7 @@ export default function DataTable() {
         }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
-      />
+        />
     </div>
   );
 }
