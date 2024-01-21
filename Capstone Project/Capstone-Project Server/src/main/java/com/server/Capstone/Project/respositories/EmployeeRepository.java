ca68c5311375e9ab5e463064c9279023014509f6
Emily Reid <emily.reid31@gmail.com>
2024-01-21 18:39:21 +0000

    filed client & server folders within the Capstone folder

--- Capstone Project/Capstone-Project Server/src/main/java/com/server/Capstone/Project/respositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.server.Capstone.Project.respositories;
-
-import com.server.Capstone.Project.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository <Employee , Long > {
-}
